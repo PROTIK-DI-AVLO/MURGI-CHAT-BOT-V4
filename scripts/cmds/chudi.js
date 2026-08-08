c2549ca9471ddb08a45afed0c94621f26af2c7f4
PROTIK-DI-AVLO <xyzxand68@gmail.com>
2026-08-08 15:20:16 +0600

    Update chudi.js

--- scripts/cmds/chudi.js
+++ scripts/cmds/chudi.js
@@ -10,7 +10,7 @@ module.exports.config = {
 }
 module.exports.onStart = async function({ api, args, Users, event}) {
     var mention = Object.keys(event.mentions)[0];
-    if(!mention) return api.sendMessage("BADHON BOSS!খানকিরপুলারে একটা মেনশন দেন-!!🫂", event.threadID);
+    if(!mention) return api.sendMessage("PROTIK BOSS!খানকিরপুলারে একটা মেনশন দেন-!!🫂", event.threadID);
  let name =  event.mentions[mention];
     var arraytag = [];
         arraytag.push({id: mention, tag: name});
